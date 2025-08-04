45694f68570ba469fb76e1a64e8ff1bdda43cef6
FerasA10 <fahz1499@gmail.com>
2025-08-04 18:38:09 +0300

    Update calc.py
    Signed-off-by: FerasA10 <fahz1499@gmail.com>

--- calc.py
+++ calc.py
@@ -19,12 +19,26 @@ if op == "+":
 elif op == "-":
     print("The value is :"+str(num1-num2))
 #الضرب 
+elif op == "*":
+    print("The value is "+str(pow(num1,num2)))
 #القسمة
+elif op =="/":
+    print("The value is "+str(num1/num2))
 #المتبقي
+elif op == "%":
+    print("The value is "+str(num1%num2))
 #الاس 
+elif op == "^":
+    print("The value is "+str(num1^num2))
 #القيمة المطلقة
+elif op == "absolute value":
+    print("The value is "+str(num1,num2))
 #التقريب
+elif op == "round":
+    print("The value is "+str(num1,num2))
 #الجذر التربيعي
+elif op == "square root":
+    print("The value is "+str(num1,num2))
 #المساعدات
 else:
     print("Error...")
