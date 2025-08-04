83cfa1c6a139418714175162113b1835f69f11ca
marwanxd7 <115586469+marwanxd7@users.noreply.github.com>
2025-08-04 20:47:06 +0300

    Update calc.py
    Signed-off-by: marwanxd7 <115586469+marwanxd7@users.noreply.github.com>

--- calc.py
+++ calc.py
@@ -1,3 +1,4 @@
+from math import *
 #الواجهة 
 print("**********************************************************************************************************************")
 print("******####******####********#*********####******##*****##******#********####**********#**********####******##*###*****")
@@ -5,47 +6,54 @@ print("****##********##****##******#*******##**********##*****##******#******##*
 print("****##********##****##******#*******##**********##*****##******#******##****##********#********##****##******#********")
 print("******####******####*##*****##********####********#####********##*******####*##*******###********####********#********")
 print("**********************************************************************************************************************\n")
-
+#المساعدات
 print("The oprations that you can use : | + | - | * | / | remainder of division | ^ | absolute value | round | square root |")
 
 #المتغيرات
-from math import *
-num1= float(input ("enter the first numper: "))
-op = input ("enter the operation")
-if op in ["absolute value", "round", "square root"]:
-    num1= float(input ("enter the first numper"))
-else:
+
+op = input("enter the operation: ")
+
+if op in ["+" , "-" , "*" , "/" , "remainder of division" , "^"]:
+    num1= float(input ("enter the first numper: "))
     num2= float(input ("enter the second numper: "))
-#الجمع
-if op == "+":
-    print("the value " +str(num1+num2))
+   #الجمع
+    if op == "+":
+        print("the value " +str(num1+num2))
 #الطرح
-elif op == "-":
-    print("The value is "+str(num1-num2)) 
+    elif op == "-":
+        print("The value is "+str(num1-num2)) 
 #الضرب 
-elif op == "*":
-    print("The value is "+str(num1*num2))  
+    elif op == "*":
+        print("The value is "+str(num1*num2))  
 #القسمة
-elif op =="/":
-    print("The value is "+str(num1/num2))
+    elif op =="/":
+        print("The value is "+str(num1/num2))
 #المتبقي
-elif op == "%":
-    print("The value is "+str(num1%num2))
+    elif op == "%":
+        print("The value is "+str(num1%num2))
 #الاس 
-elif op == "^":
-    print("The value is "+str(pow(num1,num2)))
-#القيمة المطلقة
-elif op == "absolute value":
-    print("The value is "+str(abs(num1)))
+    elif op == "^":
+        print("The value is "+str(pow(num1,num2)))
+    else:
+        print("erro")
+
+
+
+elif op in ["absolute value" , "round" , "square root"]:
+    num1= float(input ("enter the numper: "))
+    #القيمة المطلقة
+    if op == "absolute value":
+        print("The value is "+str(abs(num1)))
 #التقريب
-elif op == "round":
-    print("The value is "+str(round(num1)))
+    elif op == "round":
+        print("The value is "+str(round(num1)))
 #الجذر التربيعي
-elif op == "square root":
-    if num1 >= 0:
-     print("The value is "+str(sqrt(num1)))
-else:
-    print("erorr,cannot take negvative numper")
-#المساعدات
+    elif op == "square root":
+        if num1 >= 0:
+            print("The value is "+str(sqrt(num1)))
+        else:
+            print("error")
+
+#error
 else:
     print("Error...")
